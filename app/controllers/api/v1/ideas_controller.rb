7039b784972fb6b4e1baef3068522ca2b5e53ace
Jason Pilz <jasonpilz@gmail.com>
2016-02-01 19:15:33 -0700

    revise .gitignore to exclude coverage/

---
+++ app/controllers/api/v1/ideas_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::IdeasController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Idea.all
+  end
+
+  def show
+    respond_with Idea.find(params[:id])
+  end
+
+  def create
+    respond_with :api, :v1, Idea.create(idea_params)
+  end
+
+  def update
+    respond_with Idea.find(params[:id]).update(idea_params)
+  end
+
+  def destroy
+    respond_with Idea.destroy(params[:id])
+  end
+
+  private
+
+    def idea_params
+      params.permit(:title, :body, :quality)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root 'ideas#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :ideas, only: [:index, :show, :destroy, :update, :create], defaults: { format: :json } do
+
+      end
+    end
+  end
 end
