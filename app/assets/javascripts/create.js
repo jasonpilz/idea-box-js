9a9091af9d2cc4c271ec1b1dd6121455a3473518
Jason Pilz <jasonpilz@gmail.com>
2016-02-02 16:00:58 -0700

    add functionality to view all ideas.
    - some refactoring done to create.js as per Steves recommendations

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,32 +1,31 @@
 $(() => {
-    createIdea()
+    var $titleField = $('#title-field');
+    var $bodyField = $('#body-field');
+
+    bindEventsToNewIdeaForm()
 })
 
-var createIdea = () => {
+var bindEventsToNewIdeaForm = () => {
     $('#save-idea').click( (event) => {
         event.preventDefault()
         var ideaParams = {
+            // title: $titleField.val(),
+            // body:  $bodyField.val()
             title: $('#title-field').val(),
             body:  $('#body-field').val()
         }
 
-        $.ajax({
-            type: "POST",
-            url: "api/v1/ideas",
-            data: ideaParams,
-            success: (data) => {
-                clearFields()
-                // clearIdeas()
-                // loadIdeas()
-            },
-            error: (xhr) => {
-                alert(xhr.responseText)
-            }
+        $.post('/api/v1/ideas', ideaParams)
+            .then(clearFields())
+            .then(clearIdeas())
+            .then(fetchIdeas())
+            .fail(xhr => alert(xhr.responseText));
         })
-    })
 }
 
 var clearFields = () => {
+    // $titleField.val("")
+    // $bodyField.val("")
     $('#title-field').val("")
     $('#body-field').val("")
 }
---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,48 @@
+$(() => {
+    fetchIdeas()
+})
+
+var fetchIdeas = () => {
+    $.getJSON("/api/v1/ideas", function(data) {
+        chronoSortIdeas(data)
+    })
+}
+
+var chronoSortIdeas = (data) => {
+    data.sort((a, b) => {
+        return b.id - a.id
+    })
+    .forEach((idea) => {
+        renderIdea(idea)
+    })
+}
+
+var renderIdea = (idea) => {
+    $('#ideas').append(
+        `<div id='idea'>`
+        + `<h3 contenteditable='true' class='well idea-title' id='title' idea-id='${idea.id}' tile=${idea.title}>`
+        + `${idea.title}`
+        + `</h3><p contenteditable='true' class='well' id='body' idea-id=${idea.id} >`
+        + `${truncate(idea.body)}`
+        + `</p>`
+        + `<p class='well'>`
+        + `Quality: ${idea.quality}`
+        + `</p>`
+        + `<br>`
+        + `<button type="button" class="btn btn-danger" id="delete-idea" idea-id=${idea.id}>Delete</button>`
+        + `<hr>`
+        + `</div>`
+    )
+}
+
+var clearIdeas = () => {
+    $('#ideas').children().remove();
+}
+
+var truncate = (string) => {
+    if (string.length > 100) {
+        return $.trim(string).substring(0, 100).split(" ").slice(0, -1).join(" ") + "...";
+    } else {
+        return string;
+    }
+}
