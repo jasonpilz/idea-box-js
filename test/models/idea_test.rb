2c8cceaa58bc48c1de8d7ad0cdfeb47f5951c271
Jason Pilz <jasonpilz@gmail.com>
2016-02-02 08:18:19 -0700

    add initial model tests for idea

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -1,4 +1,27 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
+  test "it gets created with default values" do
+    idea = Idea.create
+
+    assert_equal nil, idea.title
+    assert_equal nil, idea.body
+    assert_equal "Swill", idea.quality
+  end
+
+  test "it can be created with quality level of plausible" do
+    idea = Idea.create(quality: 1)
+
+    assert_equal nil, idea.title
+    assert_equal nil, idea.body
+    assert_equal "Plausible", idea.quality
+  end
+
+  test "it can be created with quality level of genius" do
+    idea = Idea.create(quality: 2)
+
+    assert_equal nil, idea.title
+    assert_equal nil, idea.body
+    assert_equal "Genius", idea.quality
+  end
 end
