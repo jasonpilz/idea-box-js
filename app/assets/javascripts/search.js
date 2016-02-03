b562f4f80b57cf99e9b6d36e1ebbef5aba055498
Jason Pilz <jasonpilz@gmail.com>
2016-02-02 20:24:27 -0700

    add initial implementation for real-time idea filtering

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,10 +1,16 @@
-(() => {
+$(() => {
     filterIdeas();
 })
 
 var filterIdeas = () => {
-    console.log('yes by')
-    $('.search-field').keyup("change", function(event) {
-        console.log("keyup")
+    $('.search-field').keyup(function(event) {
+        var search = $(this).val().toLowerCase();
+        var ideas = $('#ideas').children();
+        ideas.removeClass('hidden');
+        var toHide = ideas.filter(function() {
+            var all = $(this).children('.well').text().toLowerCase();
+            return !(all.includes(search));
+        })
+        toHide.addClass("hidden");
     })
 }
